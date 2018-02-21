f6d4982d9a55dbd655b7de87210a1e84bb712dba
Josselin BUILS <josselin.buils@gmail.com>
2018-02-21 20:42:47 +0100

    Auto format using uncrustify

--- src/Params.h
+++ src/Params.h
@@ -1,17 +1,17 @@
 /* Copyright 2017 Streampunk Media Ltd.
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 
 #ifndef PARAMS_H
 #define PARAMS_H
@@ -23,61 +23,63 @@ using namespace v8;
 namespace streampunk {
 
 class Params {
-  protected:
-  Params() {}
-  virtual ~Params() {}
+protected:
+Params() {
+}
+virtual ~Params() {
+}
 
-  Local<Value> getKey(Local<Object> tags, const std::string& key) {
-    Local<Value> val = Nan::Null();
-    Local<String> keyStr = Nan::New<String>(key).ToLocalChecked();
+Local<Value> getKey(Local<Object> tags, const std::string& key) {
+	Local<Value> val = Nan::Null();
+	Local<String> keyStr = Nan::New<String>(key).ToLocalChecked();
 
-    if (Nan::Has(tags, keyStr).FromJust()) {
-      val = Nan::Get(tags, keyStr).ToLocalChecked();
-    }
+	if (Nan::Has(tags, keyStr).FromJust()) {
+		val = Nan::Get(tags, keyStr).ToLocalChecked();
+	}
 
-    return val;
-  }
+	return val;
+}
 
-  bool unpackBool(Local<Object> tags, const std::string& key, bool dflt) {
-    bool result = dflt;
-    Local<Value> val = getKey(tags, key);
+bool unpackBool(Local<Object> tags, const std::string& key, bool dflt) {
+	bool result = dflt;
+	Local<Value> val = getKey(tags, key);
 
-    if (Nan::Null() != val) {
-      result = Nan::To<bool>(val).FromJust();
-    }
+	if (Nan::Null() != val) {
+		result = Nan::To<bool>(val).FromJust();
+	}
 
-    return result;
-  }
+	return result;
+}
 
-  uint32_t unpackNum(Local<Object> tags, const std::string& key, uint32_t dflt) {
-    uint32_t result = dflt;
-    Local<Value> val = getKey(tags, key);
+uint32_t unpackNum(Local<Object> tags, const std::string& key, uint32_t dflt) {
+	uint32_t result = dflt;
+	Local<Value> val = getKey(tags, key);
 
-    if (Nan::Null() != val) {
-      result = Nan::To<uint32_t>(val).FromJust();
-    }
+	if (Nan::Null() != val) {
+		result = Nan::To<uint32_t>(val).FromJust();
+	}
 
-    return result;
-  } 
+	return result;
+}
 
-  std::string unpackStr(Local<Object> tags, const std::string& key, std::string dflt) {
-    std::string result = dflt;
-    Local<Value> val = getKey(tags, key);
+std::string unpackStr(Local<Object> tags, const std::string& key, std::string dflt) {
+	std::string result = dflt;
+	Local<Value> val = getKey(tags, key);
 
-    if (Nan::Null() != val) {
-      result = *String::Utf8Value(val);
-    }
+	if (Nan::Null() != val) {
+		result = *String::Utf8Value(val);
+	}
 
-    return result;
-  }
+	return result;
+}
 
-  std::string unpackValue(Local<Value> val) {
-    Local<Array> valueArray = Local<Array>::Cast(val);
-    return *String::Utf8Value(valueArray->Get(0));
-  }
+std::string unpackValue(Local<Value> val) {
+	Local<Array> valueArray = Local<Array>::Cast(val);
+	return *String::Utf8Value(valueArray->Get(0));
+}
 
-  private:
-  Params(const Params &);
+private:
+Params(const Params &);
 };
 
 } // namespace streampunk
--- src/naudiodon.cc
+++ src/naudiodon.cc
@@ -1,17 +1,17 @@
 /* Copyright 2017 Streampunk Media Ltd.
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 
 #include <nan.h>
 #include "GetDevices.h"
@@ -19,15 +19,13 @@
 
 NAN_MODULE_INIT(Init) {
 
-  streampunk::OutContext::Init(target);
+	Nan::Set(
+		target,
+		Nan::New("getDevices").ToLocalChecked(),
+		Nan::GetFunction(Nan::New<v8::FunctionTemplate>(streampunk::GetDevices)).ToLocalChecked()
+		);
 
-  Nan::Set(
-    target,
-    Nan::New("getDevices").ToLocalChecked(),
-    Nan::GetFunction(
-      Nan::New<v8::FunctionTemplate>(streampunk::GetDevices)
-    ).ToLocalChecked()
-  );
+	streampunk::OutContext::Init(target);
 }
 
 NODE_MODULE(portAudio, Init);
